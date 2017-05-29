6527896f7e46c0b1492f8decc601377a27516365
Benjamin Schubert <ben.c.schubert@gmail.com>
2017-05-29 22:48:23 +0200

    added handling of uniqueness in registration

--- server/app/controllers/Registration.scala
+++ server/app/controllers/Registration.scala
@@ -1,15 +1,16 @@
 package controllers
 
+import java.sql.SQLIntegrityConstraintViolationException
 import javax.inject.Inject
 
-import play.api.mvc.{Action, Controller}
 import forms.RegistrationForm._
 import managers.UserManager
 import models.User
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, Controller}
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 
 class Registration @Inject()(users: UserManager, val messagesApi: MessagesApi) extends Controller with I18nSupport {
@@ -25,6 +26,15 @@ class Registration @Inject()(users: UserManager, val messagesApi: MessagesApi) e
       data => {
         users.create(User(None, data.name, data.password))
           .map(_ => Redirect("/"))
+          .recover({
+            case exception: SQLIntegrityConstraintViolationException =>
+              if (exception.getMessage.contains("Duplicate") && exception.getMessage.contains("username")) {
+                BadRequest(views.html.registration(form.withError("username", "This username is already taken")))
+              } else {
+                // FIXME: we should log this
+                InternalServerError("Oops ! Something bad happened while saving you to the database. Please retry.")
+              }
+          })
       }
     )
   }
