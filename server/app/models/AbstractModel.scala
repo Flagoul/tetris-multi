68d3470647b49d5db1a84e897d58581e9a5fd8e0
Benjamin Schubert <ben.c.schubert@gmail.com>
2017-06-03 17:52:37 +0200

    finished documenting modesl

--- server/app/models/AbstractModel.scala
+++ server/app/models/AbstractModel.scala
@@ -3,11 +3,28 @@ package models
 import managers.DBWrapper.api._
 
 
+/**
+  * Base class to represent any model to be stored in the database.
+  *
+  * @param id unique identifier of the model
+  */
 abstract class AbstractModel(id: Option[Long])
 
 
-abstract class AbstractTable[AbstractModel](_tableTag: Tag, _tableName: String)
-  extends Table[AbstractModel](_tableTag, _tableName) {
+/**
+  * Represent any table that is stored in the database.
+  *
+  * @param tag to give to the table
+  * @param name of the table
+  * @tparam AbstractModel model stored in the table
+  */
+abstract class AbstractTable[AbstractModel](tag: Tag, name: String)
+  extends Table[AbstractModel](tag, name) {
 
+  /**
+    * Id column in the table
+    *
+    * @return the id column
+    */
   def id: Rep[Long] = column[Long]("id", O.PrimaryKey, O.AutoInc)
 }
--- server/app/models/UserSession.scala
+++ server/app/models/UserSession.scala
@@ -3,19 +3,61 @@ package models
 import java.sql.Timestamp
 
 import managers.DBWrapper.api._
+import slick.lifted.{ForeignKeyQuery, ProvenShape}
 
 
-case class UserSession(id: Option[Long], uuid: String, expiration: Option[Timestamp], user_id: Long) extends AbstractModel(id)
+/**
+  * Representation of the UserSession model.
+  *
+  * @param id unique identifier of the model
+  * @param uuid to store on the http session to access the session
+  * @param expiration date of expiration for this session
+  * @param user_id for which this session is valid
+  */
+case class UserSession(id: Option[Long], uuid: String, expiration: Option[Timestamp], user_id: Long)
+  extends AbstractModel(id)
 
 
+/**
+  * Table in which to store sessions
+  *
+  * @param tag to give to the table
+  */
 class SessionTable(tag: Tag) extends AbstractTable[UserSession](tag, "sessions") {
-  val users = TableQuery[UserTable]
+  /**
+    * Table containing users for joining
+    */
+  val users: TableQuery[UserTable] = TableQuery[UserTable]
 
-  def uuid = column[String]("uuid", O.Unique)
-  def expiration = column[Timestamp]("expiration")
-  def user_id = column[Long]("user_id")
+  /**
+    * Column containing the uuid.
+    *
+    * @return the column containing the uuid.
+    */
+  def uuid: Rep[String] = column[String]("uuid", O.Unique)
 
-  def user = foreignKey("user_fk", user_id, users)(_.id, onUpdate=ForeignKeyAction.Cascade, onDelete=ForeignKeyAction.Cascade)
+  /**
+    * Column containing the expiration date
+    *
+    * @return the column containing the expiration date
+    */
+  def expiration: Rep[Timestamp] = column[Timestamp]("expiration")
 
-  override def * = (id.?, uuid, expiration.?, user_id) <> (UserSession.tupled, UserSession.unapply)
+  /**
+    * Column containing the user id to which the sesison refer.
+    *
+    * @return the column containing the user id
+    */
+  def user_id: Rep[Long] = column[Long]("user_id")
+
+  /**
+    * User that the session references
+    *
+    * @return the foreign key to the user.
+    */
+  def user: ForeignKeyQuery[UserTable, User] =
+    foreignKey("user_fk", user_id, users)(_.id, onUpdate=ForeignKeyAction.Cascade, onDelete=ForeignKeyAction.Cascade)
+
+  override def * : ProvenShape[UserSession] =
+    (id.?, uuid, expiration.?, user_id) <> (UserSession.tupled, UserSession.unapply)
 }
