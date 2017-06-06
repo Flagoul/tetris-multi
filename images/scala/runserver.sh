912650c2ce3b855fc2b24316a73d651d27e9eef0
Benjamin Schubert <ben.c.schubert@gmail.com>
2017-06-06 20:52:16 +0200

    automagically apply migrations

--- images/scala/runserver.sh
+++ images/scala/runserver.sh
@@ -4,4 +4,4 @@ set -eux
 
 rm -f /srv/app/server/target/universal/stage/RUNNING_PID
 
-/srv/app/server/target/universal/stage/bin/server
+/srv/app/server/target/universal/stage/bin/server -Dplay.evolutions.db.default.autoApply=true
