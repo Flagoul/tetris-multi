85002ddb16a0d1710cf3529d83abd2c2729ba390
Benjamin Schubert <ben.c.schubert@gmail.com>
2017-06-06 20:55:51 +0200

    updated deployment config
    fix #12

--- images/scala/runserver.sh
+++ images/scala/runserver.sh
@@ -4,4 +4,9 @@ set -eux
 
 rm -f /srv/app/server/target/universal/stage/RUNNING_PID
 
-/srv/app/server/target/universal/stage/bin/server -Dplay.evolutions.db.default.autoApply=true
+if [ -z ${TETRIS_SECRET} ]; then
+    echo "Production Secret not set, aborting" >&2
+    exit 1
+fi
+
+/srv/app/server/target/universal/stage/bin/server -Dplay.evolutions.db.default.autoApply=true -Dplay.crypto.secret=${TETRIS_SECRET}
